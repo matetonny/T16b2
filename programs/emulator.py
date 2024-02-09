7bb9c1163ff23ea532b0e08eef389e8007411926
Mate Chocheli <matechocheli2@gmail.com>
2024-02-09 16:29:58 +0400

    added a few instructions to the emulator

--- programs/emulator.py
+++ programs/emulator.py
@@ -11,6 +11,8 @@ registers = {
     '111': 0,
 }
 
+ram = {}
+
 class Instruction:
     def __init__(self, instruction: bytearray) -> None:
         self.instruction = bin(instruction[0]).replace('0b', '').rjust(8, '0')
@@ -31,7 +33,48 @@ def run_instruction(instruction: Instruction):
     match instruction.instruction:
         case '00000000':
             registers[instruction.er1] = registers['000']
-
+        case '00000001':
+            registers[instruction.er1] = registers['001']
+        case '00000010':
+            registers[instruction.er1] = registers['010']
+        case '00000011':
+            registers[instruction.er1] = registers['110']
+        case '00000100':
+            if instruction.reg_or_diri == '0':
+                registers['000'] = registers[instruction.er2]
+            else:
+                registers['000'] = int(instruction.diri, 2)
+        case '00000101':
+            if instruction.reg_or_diri == '0':
+                registers['001'] = registers[instruction.er2]
+            else:
+                registers['001'] = int(instruction.diri, 2)
+        case '00000110':
+            registers['011'] = instruction.diri 
+            registers[instruction.er2] = ram[registers['011']]
+        case '00000111':
+            registers['011'] = instruction.diri 
+            ram[registers['011']] = registers[instruction.er2]
+        case '00001000':
+            if instruction.reg_or_diri == '0':
+                result = bin(registers[instruction.er1] + registers[instruction.er2])[2:].rjust(17, '0')
+                registers['010'] = int(result[1:], 2)
+                registers['110'] = int(result[0])
+            else:
+                result = bin(int(instruction.diri, 2) + registers[instruction.er2])[2:].rjust(17, '0')
+                registers['010'] = int(result[1:], 2)
+                registers['110'] = int(result[0])
+        case '00001000':
+            if instruction.reg_or_diri == '0':
+                result = bin(registers[instruction.er1] - registers[instruction.er2]).rjust(17, '0')
+                registers['010'] = int(result[2:], 2)
+                registers['110'] = int(result[0])
+            else:
+                result = bin(int(instruction.diri, 2) - registers[instruction.er2]).rjust(17, '0')
+                registers['010'] = int(result[2:], 2)
+                registers['110'] = int(result[0])
+                
+        
 def run_program(program: bytes):
     ...
 
@@ -39,6 +82,15 @@ cwd = os.getcwd()
 boot_program_file = open(cwd + '/programs/boot.hex', 'rb')
 boot_program = boot_program_file.read()
 
-Instruction(read_instruction(0, boot_program))
+my_instruction = Instruction(b'\x00\x00\x00\x00')
+my_instruction.instruction = '00001000'
+my_instruction.diri = '1100000000000000'
+my_instruction.er1 = '000'
+my_instruction.er2 = '001'
+my_instruction.reg_or_diri = '1'
+
+registers['001'] = int('1100000000000000', 2)
+run_instruction(my_instruction)
+print(bin(registers['010']), bin(registers['110']))
 
 boot_program_file.close()
