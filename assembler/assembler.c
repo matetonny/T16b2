1b484c5c64306212845972a4fbf6336341a60a5b
Mate Chocheli <matechocheli2@gmail.com>
2024-06-19 14:46:34 +0400

    add other token types

--- assembler/assembler.c
+++ assembler/assembler.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "assembler.h"
 
 int main(int argc, char const *argv[]){
@@ -9,7 +11,7 @@ int main(int argc, char const *argv[]){
 
     // exit if file doesnt open
     if (pfile == NULL){
-        printf("Not able to open the file.");
+        printf("\e[0;31merror: Not able to open the file.");
         return 1;
     }
 
@@ -26,11 +28,40 @@ int main(int argc, char const *argv[]){
         }
     }
 
-    for (int i = 0; i < counter + 1; i++){
-        printf("%s", input_program[i]);
-    }
-
     fclose(pfile);
+
+    char* str;
+    const char* delim = " ";
+    int num_tokens;
+    char** tokens;
+    token tokenized_program[] = {};
+    int token_counter = 0;
+
+    // loop through the input program and tokenize it
+    for (int i = 0; i < counter + 1; i++){
+        str = input_program[i];
+        tokens = split(str, delim, &num_tokens);
+
+        for (int j = 0; j < num_tokens; j++){
+            if (is_instruction(tokens[j])){
+                tokenized_program[token_counter].token_type = 'f';
+            } else if (starts_with(tokens[j], "0x") || is_number(tokens[j])) {
+                tokenized_program[token_counter].token_type = 'i';
+            } else if (is_register(tokens[j])) {
+                tokenized_program[token_counter].token_type = 'r';
+            } else if (starts_with(tokens[j], "#")) {
+                tokenized_program[token_counter].token_type = 'm';
+            } else {
+                printf("\e[0;31merror: token with no type");
+                return 1;
+            }
+
+            strcpy(tokenized_program[token_counter].value, tokens[j]);
+            printf("%s, %c, %d\n", tokenized_program[token_counter].value, tokenized_program[token_counter].token_type, token_counter);
+
+            token_counter++;
+        }
+    }
 
     return 0;
 }
